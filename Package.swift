258a202a5c01482c85bb0ab78fa7cfcda2fa02bf
John Bushnell <jbushnell@kochava.com>
2020-09-22 17:21:57 -0700

    4.0.0-beta.5

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(
             name: "KochavaCore",
             url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaCore",
-            from: "4.0.0-beta.4"
+            from: "4.0.0-beta.5"
         ),
     ],
     targets:
