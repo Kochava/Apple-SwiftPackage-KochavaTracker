cdca3f4aafd228de37c698b5902cfaec88a9a3c3
John Bushnell <jbushnell@kochava.com>
2021-01-21 15:40:05 -0800

    4.3.1

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(
             name: "KochavaCore",
             url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaCore",
-            from: "4.3.0"
+            from: "4.3.1"
         ),
     ],
     targets:
